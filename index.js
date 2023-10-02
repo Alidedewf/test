3086a4b08164c2e62eece1d429160bf9c8058fd5
Alidedewf <102351128+Alidedewf@users.noreply.github.com>
2023-10-02 16:26:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let number = 0;
+const minusButton = document.querySelector(".minus");
+const resetButton = document.querySelector(".reset");
+const randomButton = document.querySelector(".random");
+const plusButton = document.querySelector(".plus");
+
+function updateNumber() {
+  const NumberElement = document.getElementById("numberJS");
+  NumberElement.textContent = number;
+}
+
+function minus() {
+  number--;
+  updateNumber();
+}
+
+function reset() {
+  number = 0;
+  updateNumber();
+}
+
+function random() {
+  number = Math.floor(Math.random() * 2000 - 1000);
+  updateNumber();
+}
+
+function plus() {
+  number++;
+  updateNumber();
+}
+
+minusButton.addEventListener("click", minus);
+resetButton.addEventListener("click", reset);
+randomButton.addEventListener("click", random);
+plusButton.addEventListener("click", plus);
